9bda60b1d4f3cd94847ed765135923782a4044be
Aleksandr Teder <tederkedr@gmail.com>
2024-05-16 18:05:25 +0400

    added custom colors to lualine

--- lua/alexteder/plugins/lualine.lua
+++ lua/alexteder/plugins/lualine.lua
@@ -1,3 +1,47 @@
+local flavour = "mocha"
+local C = require("catppuccin.palettes").get_palette(flavour)
+local O = require("catppuccin").options
+local catppuccin = {}
+
+local transparent_bg = O.transparent_background and "NONE" or C.mantle
+
+catppuccin.normal = {
+	a = { bg = "#80abf1", fg = C.mantle, gui = "bold" },
+	b = { bg = C.surface0, fg = C.blue },
+	c = { bg = transparent_bg, fg = C.text },
+}
+
+catppuccin.insert = {
+	a = { bg = C.green, fg = C.base, gui = "bold" },
+	b = { bg = C.surface0, fg = C.green },
+}
+
+catppuccin.terminal = {
+	a = { bg = C.green, fg = C.base, gui = "bold" },
+	b = { bg = C.surface0, fg = C.green },
+}
+
+catppuccin.command = {
+	a = { bg = C.peach, fg = C.base, gui = "bold" },
+	b = { bg = C.surface0, fg = C.peach },
+}
+
+catppuccin.visual = {
+	a = { bg = C.mauve, fg = C.base, gui = "bold" },
+	b = { bg = C.surface0, fg = C.mauve },
+}
+
+catppuccin.replace = {
+	a = { bg = C.red, fg = C.base, gui = "bold" },
+	b = { bg = C.surface0, fg = C.red },
+}
+
+catppuccin.inactive = {
+	a = { bg = transparent_bg, fg = C.blue },
+	b = { bg = transparent_bg, fg = C.surface1, gui = "bold" },
+	c = { bg = transparent_bg, fg = C.overlay0 },
+}
+
 return {
 	"nvim-lualine/lualine.nvim",
 	config = function()
