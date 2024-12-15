d8f93ff44a3e2d8f617c0e31cf21f8cab684b95c
Aleksandr Teder <tederkedr@gmail.com>
2024-12-15 21:43:32 +0400

    fix conflicting keymaps

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -8,6 +8,7 @@ return {
 			on_attach = function(bufnr)
 				local api = require("nvim-tree.api")
 				api.config.mappings.default_on_attach(bufnr)
+				vim.keymap.del("n", "<C-e>", { buffer = bufnr })
 				vim.keymap.set(
 					"n",
 					"<Esc>",
