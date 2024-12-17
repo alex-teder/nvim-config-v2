ef8784bfc034a8713a75e84fcd3efe016af2d6cb
Aleksandr Teder <tederkedr@gmail.com>
2024-12-17 22:12:28 +0400

    change keymaps for quick file access

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -68,5 +68,21 @@ return {
 				vim.notify("File added.")
 			end
 		end, { noremap = true })
+
+		vim.keymap.set("n", "<leader>1", function()
+			harpoon:list():select(1)
+		end)
+		vim.keymap.set("n", "<leader>2", function()
+			harpoon:list():select(2)
+		end)
+		vim.keymap.set("n", "<leader>3", function()
+			harpoon:list():select(3)
+		end)
+		vim.keymap.set("n", "<leader>4", function()
+			harpoon:list():select(4)
+		end)
+		vim.keymap.set("n", "<leader>5", function()
+			harpoon:list():select(5)
+		end)
 	end,
 }
