404e75a6bd0fe2881fd129b595169d187c4c080d
Aleksandr Teder <a.teder@andersenlab.com>
2025-03-30 17:39:48 +0400

    re-enable diagnostics virtual text

--- lua/sets.lua
+++ lua/sets.lua
@@ -26,6 +26,8 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	end,
 })
 
+vim.diagnostic.config({ virtual_text = true })
+
 vim.fn.setreg("u", "ouseEffect(() => {}, []);jkIjkl")
 vim.fn.setreg("s", "oconst [_, set_] = useState();jk6bh")
 vim.fn.setreg("l", 'yoconsole.log()jki"", jkla;jkbblplllpIjk')
