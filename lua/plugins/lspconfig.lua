1e0b2cb2b18447000d45a9e362b0264f73ab6bfd
Aleksandr Teder <tederkedr@gmail.com>
2025-05-26 20:33:40 +0400

    refactor lspconfig

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -5,42 +5,5 @@ return {
 		{ "antosha417/nvim-lsp-file-operations", config = true },
 		"hrsh7th/cmp-nvim-lsp",
 	},
-	config = function()
-		vim.api.nvim_create_autocmd("LspAttach", {
-			callback = function(ev)
-				local opts = { buffer = ev.buf, silent = true }
-				local keymap = vim.keymap
-
-				opts.desc = "Show LSP references"
-				keymap.set("n", "gr", vim.lsp.buf.references, opts) -- show definition, references
-
-				opts.desc = "Go to declaration"
-				keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- go to declaration
-
-				opts.desc = "Show LSP definitions"
-				keymap.set("n", "gD", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-
-				opts.desc = "Show LSP type definitions"
-				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-
-				opts.desc = "See available code actions"
-				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
-
-				opts.desc = "Smart rename"
-				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- see available code actions, in visual mode will apply to selection
-
-				opts.desc = "Show line diagnostics"
-				keymap.set("n", "<leader><space>", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-				opts.desc = "Show documentation for what is under cursor"
-				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
-			end,
-		})
-
-		-- TODO: move this
-		-- Add border to the diagnostic popup window
-		vim.diagnostic.config({
-			float = { border = "single" },
-		})
-	end,
+	config = function() end,
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,6 +9,8 @@ return {
 		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
 		local mason_tool_installer = require("mason-tool-installer")
+		local lspconfig = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 		-- enable mason and configure icons
 		mason.setup({
@@ -21,29 +23,9 @@ return {
 			},
 		})
 
-		local ensure_installed = {
-			"ts_ls",
-			"html",
-			"cssls",
-			"lua_ls",
-			"graphql",
-			"emmet_language_server",
-			"pyright",
-			"vue_ls",
-			"rust_analyzer",
-		}
-
-		mason_lspconfig.setup({
-			automatic_enable = false, -- hack
-			ensure_installed = ensure_installed,
-		})
-
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"prettierd",
-				"stylua",
-				"eslint_d",
-			},
+		-- Add border to the diagnostic popup window
+		vim.diagnostic.config({
+			float = { border = "single" },
 		})
 
 		-- Add the border on hover and on signature help popup window
@@ -51,16 +33,6 @@ return {
 			["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" }),
 			["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" }),
 		}
-
-		local lspconfig = require("lspconfig")
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-		local function setup_default_handlers(server_name)
-			lspconfig[server_name].setup({
-				capabilities = capabilities,
-				handlers = handlers,
-			})
-		end
 
 		local servers = {
 			["cssls"] = function()
@@ -151,6 +123,34 @@ return {
 			end,
 		}
 
+		local ensure_installed = {
+			"ts_ls",
+			"html",
+			"cssls",
+			"lua_ls",
+			"graphql",
+			"emmet_language_server",
+			"pyright",
+			"vue_ls",
+			"rust_analyzer",
+		}
+
+		mason_tool_installer.setup({
+			ensure_installed = { "prettierd", "stylua", "eslint_d" },
+		})
+
+		mason_lspconfig.setup({
+			automatic_enable = false,
+			ensure_installed = ensure_installed,
+		})
+
+		local function setup_default_handlers(server_name)
+			lspconfig[server_name].setup({
+				capabilities = capabilities,
+				handlers = handlers,
+			})
+		end
+
 		-- do the thing
 		for _, server_name in pairs(ensure_installed) do
 			if servers[server_name] then
@@ -159,5 +159,36 @@ return {
 				setup_default_handlers(server_name)
 			end
 		end
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function(ev)
+				local opts = { buffer = ev.buf, silent = true }
+				local keymap = vim.keymap
+
+				opts.desc = "Show LSP references"
+				keymap.set("n", "gr", vim.lsp.buf.references, opts) -- show definition, references
+
+				opts.desc = "Go to declaration"
+				keymap.set("n", "gd", vim.lsp.buf.definition, opts) -- go to declaration
+
+				opts.desc = "Show LSP definitions"
+				keymap.set("n", "gD", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+
+				opts.desc = "Show LSP type definitions"
+				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+
+				opts.desc = "See available code actions"
+				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+
+				opts.desc = "Smart rename"
+				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- see available code actions, in visual mode will apply to selection
+
+				opts.desc = "Show line diagnostics"
+				keymap.set("n", "<leader><space>", vim.diagnostic.open_float, opts) -- show diagnostics for line
+
+				opts.desc = "Show documentation for what is under cursor"
+				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+			end,
+		})
 	end,
 }
