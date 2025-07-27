e71a1b67795ca50f644074233e446e295f8e2e98
Aleksandr Teder <tederkedr@gmail.com>
2025-07-27 23:43:52 +0400

    adjust lualine el placement and truncation

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -29,11 +29,26 @@ return {
 				{
 					"branch",
 					color = { fg = C.overlay0 },
-					padding = { left = 0, right = 1 },
+					padding = { left = 0, right = 0 },
+					fmt = function(str)
+						local filename = vim.fn.expand("%:t") -- Current filename
+						local filename_len = #filename
+						--
+						-- Adjust max_length based on filename length
+						local available_space = vim.o.columns -- Total statusline space
+						local other_components = 60 -- Space for other statusline components
+						local min_desired_length = 9
+
+						local max_length =
+							math.max(min_desired_length, available_space - other_components - filename_len)
+						if #str > max_length then
+							return str:sub(1, max_length - 3) .. "..."
+						end
+						return str
+					end,
 				},
 			},
 			lualine_x = {
-				"encoding",
 				{
 					"lsp_status",
 					icon = "",
@@ -43,9 +58,8 @@ return {
 						done = "",
 					},
 				},
-				"filetype",
 			},
-			lualine_y = { "progress" },
+			lualine_y = { "filetype" },
 			lualine_z = { "location" },
 		},
 	},
