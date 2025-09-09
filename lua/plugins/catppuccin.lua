c61cec9f19c563bc19c07ba96417a3c8ac18129d
Aleksandr Teder <aleksandr.teder@wayflyer.com>
2025-09-09 12:14:22 +0400

    make colorscheme overrides

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -7,6 +7,21 @@ return {
 			flavour = "mocha",
 			no_bold = true,
 			-- transparent_background = true,
+			custom_highlights = function(colors)
+				return {
+					["@variable.member"] = { fg = colors.lavender },
+					["@module"] = { fg = colors.lavender, style = { "italic" } },
+					["@type.builtin"] = { fg = colors.yellow, style = { "italic" } },
+					["@property"] = { fg = colors.lavender }, -- Same as TSField.
+					["@constructor"] = { fg = colors.sapphire },
+					["@keyword.operator"] = { link = "Operator" },
+					["@keyword.export"] = { fg = colors.sky },
+					["@tag"] = { fg = colors.mauve },
+					["@tag.builtin"] = { fg = colors.mauve },
+					["@tag.attribute"] = { fg = colors.teal, style = { "italic" } },
+					["@tag.delimiter"] = { fg = colors.sky },
+				}
+			end,
 		}
 		require("catppuccin").setup(opts)
 		vim.cmd("colorscheme catppuccin")
