6900cce108b965b0e7d1a81408294e9ef00f8b91
Aleksandr Teder <aleksandr.teder@wayflyer.com>
2025-10-09 13:24:32 +0400

    add completions toggle autocmd

--- lua/plugins/augment.lua
+++ lua/plugins/augment.lua
@@ -1,1 +1,6 @@
+vim.api.nvim_create_user_command("AugmentToggleCompletions", function()
+	vim.g.augment_disable_completions = not vim.g.augment_disable_completions
+	print("Augment completions " .. tostring(not vim.g.augment_disable_completions and "enabled" or "disabled"))
+end, {})
+
 return { "augmentcode/augment.vim" }
