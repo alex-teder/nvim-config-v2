9d2c3718628507220a708cc010a6906f9ec45475
Aleksandr Teder <aleksandr.teder@wayflyer.com>
2026-03-27 10:06:50 +0400

    set up copilot.lua

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,18 +1,18 @@
 return {
 	"zbirenbaum/copilot.lua",
+	enabled = true,
 	event = { "InsertEnter" },
-	dependencies = {
-		"copilotlsp-nvim/copilot-lsp",
-	},
 	config = function()
 		require("copilot").setup({
 			suggestion = {
-				enabled = false,
-			},
-			nes = {
 				enabled = true,
-				keymap = {},
+				auto_trigger = true,
+				keymap = {
+					accept = "<tab>",
+				},
 			},
+			panel = { enabled = false },
+			nes = { enabled = false },
 		})
 	end,
 }
