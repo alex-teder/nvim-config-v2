fc40b66787fe72bbe6cba0423ecdf4a46a2e7e1d
Aleksandr Teder <aleksandr.teder@wayflyer.com>
2026-04-17 14:01:03 +0400

    comment out copilot load on event

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,7 +1,7 @@
 return {
 	"zbirenbaum/copilot.lua",
 	enabled = true,
-	event = { "InsertEnter" },
+	-- event = { "InsertEnter" },
 	config = function()
 		require("copilot").setup({
 			suggestion = {
